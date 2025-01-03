cf8873cc3f5e58a10e984ace0c2051a74b3e2956
Przemyslaw Koziel <mos.koziel@gmail.com>
2025-01-03 15:58:53 +0100

    feat: material angular added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'eob-root',
-  imports: [RouterOutlet],
+  imports: [MatSlideToggleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
