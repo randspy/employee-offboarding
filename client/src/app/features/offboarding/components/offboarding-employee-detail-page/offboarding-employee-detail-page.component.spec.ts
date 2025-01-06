5f18829b925764b75d6b624b43db630b8ef6aeef
Przemyslaw Koziel <mos.koziel@gmail.com>
2025-01-06 14:47:25 +0100

    feat: detail page implementation

--- client/src/app/features/offboarding/components/offboarding-employee-detail-page/offboarding-employee-detail-page.component.spec.ts
+++ client/src/app/features/offboarding/components/offboarding-employee-detail-page/offboarding-employee-detail-page.component.spec.ts
@@ -1,22 +1,116 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { OffboardingEmployeeDetailPageComponent } from './offboarding-employee-detail-page.component';
+import { EmployeesStore } from '../../stores/employees.store';
+import { signal, WritableSignal } from '@angular/core';
+import { Employee } from '../../domain/employee.types';
+import { generateEmployee } from '../../../../../tests/test-object-generators';
+import { By } from '@angular/platform-browser';
 
 describe('OffboardingEmployeeDetailPageComponent', () => {
   let component: OffboardingEmployeeDetailPageComponent;
   let fixture: ComponentFixture<OffboardingEmployeeDetailPageComponent>;
+  let employees: WritableSignal<Employee[]>;
+  let mockEmployeesStore: jest.Mocked<EmployeesStore>;
 
   beforeEach(async () => {
+    employees = signal([]);
+
+    mockEmployeesStore = {
+      employees,
+      loadEmployee: jest.fn(),
+    } as unknown as jest.Mocked<EmployeesStore>;
+
     await TestBed.configureTestingModule({
       imports: [OffboardingEmployeeDetailPageComponent],
+      providers: [
+        { provide: EmployeesStore, useValue: mockEmployeesStore },
+        provideRouter([]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(OffboardingEmployeeDetailPageComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('id', 'employee-1');
     fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    employees.set([]);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call loadEmployee on init', () => {
+    expect(
+      (
+        mockEmployeesStore.loadEmployee as unknown as jest.Mock
+      ).mock.calls[0][0](),
+    ).toBe('employee-1');
+  });
+
+  it('should load employee', () => {
+    employees.set([
+      generateEmployee({
+        id: 'employee-1',
+        name: 'John Doe',
+        email: 'john.doe@example.com',
+        department: 'Department 1',
+        equipments: [
+          {
+            id: 'equipment-1',
+            name: 'Equipment 1',
+          },
+          {
+            id: 'equipment-2',
+            name: 'Equipment 2',
+          },
+        ],
+      }),
+    ]);
+
+    fixture.detectChanges();
+
+    const page = fixture.nativeElement;
+    expect(page.textContent).toContain('John Doe');
+    expect(page.textContent).toContain('john.doe@example.com');
+    expect(page.textContent).toContain('Department 1');
+    expect(page.textContent).toContain('Equipment 1');
+    expect(page.textContent).toContain('Equipment 2');
+  });
+
+  it('should have a back button', () => {
+    const backLink = fixture.debugElement.query(By.css('a'));
+
+    expect(backLink.nativeElement.textContent).toContain('Back');
+    expect(
+      backLink.nativeElement.attributes.getNamedItem('ng-reflect-router-link')
+        .value,
+    ).toContain('../');
+  });
+
+  describe('offboard button', () => {
+    it('should be enabled if employee is not offboarded', async () => {
+      employees.set([generateEmployee({ id: 'employee-1', status: 'ACTIVE' })]);
+
+      fixture.detectChanges();
+
+      const button = fixture.debugElement.query(By.css('button'));
+      expect(button.nativeElement.disabled).toBe(false);
+    });
+
+    it('should disable offboard button if employee is offboarded', async () => {
+      employees.set([
+        generateEmployee({ id: 'employee-1', status: 'OFFBOARDED' }),
+      ]);
+
+      fixture.detectChanges();
+
+      const button = fixture.debugElement.query(By.css('button'));
+      expect(button.nativeElement.disabled).toBe(true);
+    });
   });
 });
--- client/src/app/features/offboarding/components/offboarding-employee-detail-page/offboarding-employee-detail-page.component.ts
+++ client/src/app/features/offboarding/components/offboarding-employee-detail-page/offboarding-employee-detail-page.component.ts
@@ -1,10 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  input,
+  OnInit,
+} from '@angular/core';
+import { EmployeesStore } from '../../stores/employees.store';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'eob-offboarding-employee-detail-page',
-  imports: [],
+  imports: [RouterLink, MatButtonModule, MatIconModule],
   templateUrl: './offboarding-employee-detail-page.component.html',
   styleUrl: './offboarding-employee-detail-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OffboardingEmployeeDetailPageComponent {}
+export class OffboardingEmployeeDetailPageComponent implements OnInit {
+  #employeesStore = inject(EmployeesStore);
+
+  id = input.required<string>();
+  employee = computed(() =>
+    this.#employeesStore.employees().find((e) => e.id === this.id()),
+  );
+
+  ngOnInit() {
+    this.#employeesStore.loadEmployee(this.id);
+  }
+
+  isAlreadyOffboarded() {
+    return this.employee()!.status === 'OFFBOARDED';
+  }
+}
